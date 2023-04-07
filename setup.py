eb25e02d5e9d79215253cae804ca987da0664780
lestherll <ljllacuna5@gmail.com>
2023-04-07 23:26:31 +0100

    modified dep versions

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages, setup
 
 on_rtd = os.environ.get("READTHEDOCS") == "True"
 
-install_requires = ["numpy<1.23.0",
-                    "networkx<2.7",
+install_requires = ["numpy>=1.22.4",
+                    "networkx",
                     "decorator==4.4.2",
                     "cmake",
                     "Cython",
                     "tqdm",
                     "python-louvain",
-                    "pandas<=1.3.5",
+                    "pandas<2.0.0",
                     "six",
                     "scipy"]
 
